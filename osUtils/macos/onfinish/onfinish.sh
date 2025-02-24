3d3a5f0c156120ff04728fb0ea329f3d8daea76f
josePereiro <jose.pereiro.personal@gmail.com>
2025-02-24 10:40:11 -0500

    UP {2025-02-24 10:40:10}

--- osUtils/macos/onfinish/onfinish.sh
+++ osUtils/macos/onfinish/onfinish.sh
@@ -8,3 +8,8 @@ __ONFINISHED_UTILS_DIR__="${__SOURCING_ROOT__}/onfinish"
 # run git auto commit
 bash "${__ONFINISHED_UTILS_DIR__}/auto-commit.sh" > /dev/null 2>&1
 # bash "${__ONFINISHED_UTILS_DIR__}/auto-commit.sh"
+
+
+## ------------------------------------------------------------
+# Prepend a few paths
+source ${__BASH_UTILS_DIR__}/path.sh
