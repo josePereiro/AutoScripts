947e41fe44ce7bd15251956844e11cdb788483de
josePereiro <jose.pereiro.personal@gmail.com>
2025-04-30 10:05:59 -0400

    UP {2025-04-30 10:05:59}

--- osUtils/macos/bash/marker.sh
+++ osUtils/macos/bash/marker.sh
@@ -7,7 +7,9 @@ MARKER_PDF_REMOTES[1]="pereiro@10.6.230.13:/home/pereiro/reading"
 marker_sync_pdf_remote () {
     local REMOTE=${MARKER_PDF_REMOTES[$1]}
     local LOCAL=${MARKER_PDF_LOCAL_REPO[$1]}
+    echo "PUSHING"
     rsync -av --ignore-existing "${LOCAL}/" "${REMOTE}"
+    echo "PULLING"
     rsync -av --ignore-existing "${REMOTE}/" "${LOCAL}"
 }
 
@@ -17,6 +19,8 @@ MARKER_MARKDOWN_LOCAL_REPO[1]="/Users/Pereiro/marker"
 marker_sync_markdown_remote () {
     local REMOTE=${MARKER_MARKDOWN_REMOTES[$1]}
     local LOCAL=${MARKER_MARKDOWN_LOCAL_REPO[$1]}
+    echo "PULLING"
     rsync -av --ignore-existing "${REMOTE}/" "${LOCAL}"
+    echo "PUSHING"
     rsync -av --ignore-existing "${LOCAL}/" "${REMOTE}"
 }
