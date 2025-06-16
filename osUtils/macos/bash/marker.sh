4ff4687fe7d4b91a48f55e0e84d500c5cf653e54
josePereiro <jose.pereiro.personal@gmail.com>
2025-06-16 14:37:25 -0400

    UP {2025-06-16 14:37:25}

--- osUtils/macos/bash/marker.sh
+++ osUtils/macos/bash/marker.sh
@@ -20,7 +20,7 @@ marker_sync_markdown_remote () {
     local REMOTE=${MARKER_MARKDOWN_REMOTES[$1]}
     local LOCAL=${MARKER_MARKDOWN_LOCAL_REPO[$1]}
     echo "PULLING"
-    rsync -av --ignore-existing "${REMOTE}/" "${LOCAL}"
+    rsync -av --ignore-existing --exclude '.git' "${REMOTE}/" "${LOCAL}"
     echo "PUSHING"
     rsync -av --ignore-existing "${LOCAL}/" "${REMOTE}"
 }
