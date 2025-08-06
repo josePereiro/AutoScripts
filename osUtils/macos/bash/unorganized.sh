073bfc33f82a617087747bc55daac009f0e3ebaa
josePereiro <jose.pereiro.personal@gmail.com>
2025-08-06 15:07:58 +0200

    UP {2025-08-06 15:07:58}

--- osUtils/macos/bash/unorganized.sh
+++ osUtils/macos/bash/unorganized.sh
@@ -1,3 +1,21 @@
 ## ------------------------------------------------------------
 # https://docs.gitignore.io/install/command-line
 function gi() { curl -sLw "\n" https://www.toptal.com/developers/gitignore/api/$@ ;}
+
+
+sound_m4a2mp3() {
+  if [[ $# -ne 1 ]]; then
+    echo "Usage: m4a2mp3 input.m4a"
+    return 1
+  fi
+
+  infile="$1"
+  if [[ ! -f "$infile" ]]; then
+    echo "File not found: $infile"
+    return 1
+  fi
+
+  outfile="${infile%.*}.mp3"
+
+  ffmpeg -i "$infile" -c:a libmp3lame -q:a 2 "$outfile"
+}
